5939488979aa4739af416ec3215c5217377d759a
Johnson4996 <Johnson.4996@gmail.com>
2020-07-29 16:15:04 -0500

    working on notes api and rendering

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,14 +1,34 @@
+import { saveNote } from "./NoteDataProvider.js"
+
 const contentTarget = document.querySelector(".noteFormContainer")
+
+
+// Handle browser-generated click event in component
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+        const noteTitle = document.querySelector("#noteform--title")
+        const noteAuthor = document.querySelector("#noteform--author")
+        const noteContent = document.querySelector("#noteform--content")
+        // Make a new object representation of a note
+        const newNote = {
+            title: noteTitle.value,
+            author: noteAuthor.value,
+            note: noteContent.value,
+            timestamp: Date.now()
+        }
+
+        // Change API state and application state
+        saveNote(newNote)
+    }
+})
+
 
 const render = () =>{
     contentTarget.innerHTML = `
     <form class = "noteForm">
-    <label for ="date"> Date:</label>
-    <input type= "date" name="date" id="date">
-    <label for = "text"> Note:</label>
-    <input type="text" name="note" id="note">
-    <label for ="suspect">Suspect:</label>
-    <input type= "text" name="suspect" id="suspect">
+    <input type="text" id="noteForm--title" placeholder="Note title" />
+    <input type="text" id="noteForm--author" placeholder="Your name" />
+    <textarea id="noteForm--content" placeholder="Enter note"></textarea>
     <button id="saveNote">Save Note</button>
     </form>
     `
