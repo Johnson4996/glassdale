d10728ea25005de8d69b510c52b54b4495de8f2b
Johnson4996 <Johnson.4996@gmail.com>
2020-08-24 15:49:06 -0500

    added facilities to criminal card

--- scripts/criminals/CriminalHTML.js
+++ scripts/criminals/CriminalHTML.js
@@ -6,6 +6,12 @@ return `
     <p class="criminal__crime">Crime: ${criminal.conviction}</p>
     <p class="criminal__start">Term start: ${new Date(criminal.incarceration.start).toLocaleDateString('en-US')}</p>
     <p class="criminal__end">Term end: ${new Date(criminal.incarceration.end).toLocaleDateString('en-US')}</p>
+    <div>
+                <h2>Facilities</h2>
+                <ul>
+                    ${criminal.facility.map(f => `<li>${f.facilityName}</li>`).join("")}
+                </ul>
+            </div>
     <button id="associates--${criminal.id}">Associate Alibis</button>
 </section>
 <dialog class = "associatesDialog">
